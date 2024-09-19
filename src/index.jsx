db8daa2b7c7e5134878a2ed4be3a11329dd49482
Daniel Richter <drichter3697@gmail.com>
2024-09-19 17:46:12 -0500

    force light theme

--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,15 @@ import App from "./components/App/App";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("react-root"));
-
+const config = {
+  initialColorMode: 'light', // 'dark' | 'light'
+  useSystemColorMode: false,
+}
+const theme = extendTheme(config)
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Provider store={store}>
         <App />
       </Provider>
